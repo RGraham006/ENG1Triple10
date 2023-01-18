1d78dd2d061898149fc81325a92c449f0cad07e2
RGraham006 <117930274+RGraham006@users.noreply.github.com>
2023-01-18 11:23:05 +0000

    Created Map Class

---
+++ game/core/src/com/mygdx/game/Map.java
@@ -0,0 +1,11 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.maps.tiled.TiledMap;
+
+public class Map {
+    private TiledMap gameMap;
+
+    Map(TiledMap gameMap){
+        this.gameMap = gameMap;
+    }
+}
--- game/core/src/com/mygdx/game/MyGdxGame.java
+++ game/core/src/com/mygdx/game/MyGdxGame.java
@@ -1,18 +1,23 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class MyGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
+	private SpriteBatch batch;
 	Texture img;
-	
+	private OrthographicCamera  camera;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 	}
 
 	@Override
