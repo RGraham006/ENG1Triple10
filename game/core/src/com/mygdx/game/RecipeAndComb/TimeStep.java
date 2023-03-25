c95695d31d106818e10bcae46dcbe31f0211355e
Jack <jackhinton35@gmail.com>
2023-03-25 14:44:14 +0000

    Fixed recipe bug

--- game/core/src/com/mygdx/game/RecipeAndComb/TimeStep.java
+++ game/core/src/com/mygdx/game/RecipeAndComb/TimeStep.java
@@ -9,7 +9,11 @@ public class TimeStep extends Step{
     public boolean timeStep(Item item, float dt, boolean Interacted, float MaxProgress){
 
         item.progress = min(item.progress + dt, MaxProgress);
-        return item.progress == MaxProgress;
+        if(item.progress == MaxProgress){
+            item.progress = 0;
+            return true;
+        }
+        return false;
     }
 
 }
