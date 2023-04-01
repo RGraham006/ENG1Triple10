37b9bb2870d8f8924d61326e1a422eefc7635502
JV1ck3rs <jack.andrew.vickers@hotmail.co.uk>
2023-04-01 17:10:10 +0100

    Made a text field appear when the scenario mode button on the main screen is clicked. This will be moved in the future to a separate screen

--- game/core/src/com/mygdx/game/MenuScreen.java
+++ game/core/src/com/mygdx/game/MenuScreen.java
@@ -1,18 +1,26 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Input.TextInputListener;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.assets.loaders.AssetLoader;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.Align;
 import com.mygdx.game.Core.GameObjectManager;
 import com.mygdx.game.Core.RenderManager;
 
@@ -28,6 +36,8 @@ public class MenuScreen implements Screen {
   GameScreen VictoryScreen;
   TextureAtlas mainMenuAtlas;
   final MyGdxGame root;
+  boolean scenarioClicked = false;
+  TextField textField;
 
   private final TextureRegion playbtn;
   private final TextureRegion playbtnDown;
@@ -107,6 +117,21 @@ public class MenuScreen implements Screen {
     };
     playbtn.addListener(playbtnMouseListener);
 
+    // Creates a new skin using the given JSON file
+    // This will be used to create the TextField
+    Skin skin = new Skin(Gdx.files.internal("clean-crispy-ui.json"));
+    scenariobtn.addListener(new ClickListener() {
+      @Override
+      public void clicked(InputEvent event, float x, float y) {
+        textField = new TextField("", skin); // Creates a new TextField with the given text and skin
+        textField.setMessageText("Enter the maximum number of customers for the scenario mode");
+        textField.setAlignment(Align.center); // Aligns the text to the center of the TextField
+        textField.setPosition(200, 200); // Sets the position of the TextField
+        stage.addActor(textField); // Adds the TextField to the stage
+        scenarioClicked = true; // Sets the clicked flag for this button to true
+      }
+    });
+
     ChangeListener exitbtnMouseListener = new ChangeListener() {
       @Override
       public void changed(ChangeEvent event, Actor actor) {
@@ -134,6 +159,11 @@ public class MenuScreen implements Screen {
     Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
     stage.act(Gdx.graphics.getDeltaTime());
     stage.draw();
+    if (scenarioClicked) {
+      if (Gdx.input.isKeyJustPressed(Input.Keys.ENTER)) {
+        System.out.println(textField.getText()); // Prints the text from the TextField
+      }
+    }
   }
 
   /**
